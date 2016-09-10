05e8d616ee7dd0e34aef997f6ed3f1ee2e3f6aff
codekaizen <codekaizen@gmail.com>
2016-09-09 19:12:44 -0700

    Getting quiz program to run!

--- Program.cs
+++ Program.cs
@@ -23,34 +23,44 @@ namespace MathDrill
             //    f: Decide if correct and record correct answer time
             //    g: If incorrect, ask again
             // 5: Display statistics
-
-            Console.WriteLine("enter digit of practice");
+            Console.SetWindowPosition(0, 0);
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            Console.WriteLine("enter digit of practice 0-9");
             var input = Console.ReadLine();
             Int32 n;
             Int32.TryParse(input, out n);
             var rng = new Random();
             var answers = new List<TimeSpan>();
-            for (int i = 0; i < 25; i++)
+            for (int i = 0; i < 10; i++)
             {
                 var x = rng.Next(0, 10);
                 Console.WriteLine($"{n} + {x}");
                 var stopwatch = new Stopwatch();
                 stopwatch.Start();
-                var answer = Console.ReadLine();
                 Int32 a;
-                Int32.TryParse(answer, out a);
-                if (n + x == a)
+                do
                 {
-                    answers.Add(stopwatch.Elapsed);
-
-
-
-                }
-
-
+                    var answer = Console.ReadLine();
+                    Int32.TryParse(answer, out a);
+                    if (n + x == a)
+                    {
+                        answers.Add(stopwatch.Elapsed);
+                    }
+                    else
+                    {
+                        var previousColor = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("This is incorrect");
+                        Console.ForegroundColor = previousColor;
+                    }
+                } while (n + x != a);
 
 
             }
+            Console.WriteLine($"Best: {answers.Min()}");
+            Console.WriteLine($"Worst: {answers.Max()}");
+            Console.WriteLine($"avg: {TimeSpan.FromTicks(answers.Select(t => t.Ticks).Sum() / answers.Count)}");
+            Console.ReadKey();
 
 
         }
